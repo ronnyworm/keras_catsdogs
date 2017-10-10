e2e4503dc33016781e2442da470f0d2a41257c1e
Ronny Worm <r-worm@gmx.de>
2017-10-10 16:24:34 +0200

    Try simpler model from https://keras.io/getting-started/sequential-model-guide/#examples Not working yet

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, Activation, Dense, MaxPooling2D, Dropout, Flatten
+from keras.optimizers import SGD
 from keras import backend as K
 from scipy.misc import imread, imsave, imresize
 import numpy as np
@@ -17,22 +18,11 @@ img_width = 32
 img_heigth = 32
 
 
-# change image size
-for filename in glob.glob("/Users/ronny/Downloads/__new/Cats Dataset/example32/*"):
-	if os.path.isfile(filename):
-		img = imread(filename)
-		img = imresize(img, (img_width, img_heigth))
-		imsave(filename, img)
-
-print("done")
-
-sys.exit()
-
-filelist = glob.glob('/Users/ronny/Downloads/__new/Stanford Dogs Dataset/example320/*')
+filelist = glob.glob('/Users/ronny/Downloads/__new/Stanford Dogs Dataset/example32/*')
 
 dogs = np.array([np.array(imread(filename)) for filename in filelist])
 dogs_y = np.zeros((len(filelist),1))
-filelist = glob.glob('/Users/ronny/Downloads/__new/Cats Dataset/example320/*')
+filelist = glob.glob('/Users/ronny/Downloads/__new/Cats Dataset/example32/*')
 cats = np.array([np.array(imread(filename)) for filename in filelist])
 cats_y = np.ones((len(filelist),1))
 
@@ -47,26 +37,16 @@ np.random.shuffle(both_y)
 print("done loading data")
 
 model = Sequential()
-model.add(Conv2D(32, (3,3), input_shape=(img_width, img_heigth, 3)))
-model.add(Activation(K.relu))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Conv2D(32, (3,3)))
-model.add(Activation(K.relu))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Conv2D(64, (3,3)))
-model.add(Activation(K.relu))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation(K.relu))
+model.add(Dense(64, activation='relu', input_dim=3072))
 model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation(K.sigmoid))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
 
-model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
 
 print("done compiling the model")
 
