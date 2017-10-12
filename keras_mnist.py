0151ce6bba037c130d8ee7067f0988fc2db250be
Ronny Worm <r-worm@gmx.de>
2017-10-12 16:58:36 +0200

    Add Dataset Test accuracy: 0.57

--- keras_mnist.py
+++ keras_mnist.py
@@ -15,25 +15,74 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+from scipy.misc import imread, imsave, imresize
+import glob
+import numpy as np
 
 batch_size = 128
-num_classes = 10
+num_classes = 2
 epochs = 12
 
 # input image dimensions
-img_rows, img_cols = 28, 28
+img_rows, img_cols = 32, 32
+img_channels = 3
+
+# load my custom data
+filelist = glob.glob('catsdogsdataset/dogsexample32/*')
+train_dogs = np.array([np.array(imread(filename).flatten()) for filename in filelist])
+train_dogs_y = np.zeros((len(filelist),1))
+filelist = glob.glob('catsdogsdataset/dogsexample32test/*')
+test_dogs = np.array([np.array(imread(filename).flatten()) for filename in filelist])
+test_dogs_y = np.zeros((len(filelist),1))
+
+filelist = glob.glob('catsdogsdataset/catsexample32/*')
+train_cats = np.array([np.array(imread(filename).flatten()) for filename in filelist])
+train_cats_y = np.ones((len(filelist),1))
+filelist = glob.glob('catsdogsdataset/catsexample32test/*')
+test_cats = np.array([np.array(imread(filename).flatten()) for filename in filelist])
+test_cats_y = np.ones((len(filelist),1))
+
+train_both = np.concatenate((train_dogs, train_cats), axis=0)
+train_both_y = np.concatenate((train_dogs_y, train_cats_y), axis=0)
+test_both = np.concatenate((test_dogs, test_cats), axis=0)
+test_both_y = np.concatenate((test_dogs_y, test_cats_y), axis=0)
+
+np.random.seed(1)
+np.random.shuffle(train_both)
+np.random.seed(1)
+np.random.shuffle(train_both_y)
+np.random.seed(2)
+np.random.shuffle(test_both)
+np.random.seed(2)
+np.random.shuffle(test_both_y)
+
+# Testdata in right format?
+# imsave("out1.jpg", test_both[0].reshape(32,32,3))
+# imsave("out2.jpg", test_both[1].reshape(32,32,3))
+# imsave("out3.jpg", test_both[2].reshape(32,32,3))
+# imsave("out4.jpg", test_both[3].reshape(32,32,3))
+# 
+# print(test_both_y[0])
+# print(test_both_y[1])
+# print(test_both_y[2])
+# print(test_both_y[3])
 
 # the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = train_both.reshape(train_both.shape[0], img_rows,img_cols,img_channels)
+y_train = train_both_y
+x_test = test_both.reshape(test_both.shape[0], img_rows,img_cols,img_channels)
+y_test = test_both_y
+#mnist
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, img_channels)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, img_channels)
+    input_shape = (img_rows, img_cols, img_channels)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -43,9 +92,12 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
+# convert class vectors to binary class matrices (-> one-hot-vectors)
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# revise [convnets](https://www.youtube.com/watch?v=GYGYnspV230)
+# see <https://keras.io/layers/convolutional/#conv2d>
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
