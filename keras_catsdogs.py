c24980c2f4a4bc2810a6ad1f7e621077d760f68b
Ronny Worm <r-worm@gmx.de>
2017-10-19 17:38:10 +0200

    Add Tensorboard logging

--- keras_catsdogs.py
+++ keras_catsdogs.py
@@ -107,11 +107,15 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
+tbCallback = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0,  
+          write_graph=True, write_images=True)
+
 history = model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(x_test, y_test))
+          validation_data=(x_test, y_test),
+          callbacks=[tbCallback])
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
